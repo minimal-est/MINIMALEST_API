01454056f3a3e816b230fda2008612a929a6c850
31n5ang <31n5ang@gmail.com>
2025-02-06 13:31:52 +0900

    feat: 상태코드 추가

--- src/main/java/kr/minimalest/core/common/dto/ApiResponse.java
+++ src/main/java/kr/minimalest/core/common/dto/ApiResponse.java
@@ -7,37 +7,41 @@ import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
+import static org.springframework.http.HttpStatus.*;
+
 @Getter
 @Builder
 @AllArgsConstructor
 public class ApiResponse<T> {
 
+    private int statusCode;
     private String status;
     private String message;
     private T data;
     private LocalDateTime timestamp;
 
     public static <T> ApiResponse<T> success() {
-        return new ApiResponse<>(HttpStatus.OK.name(), "success", null, LocalDateTime.now());
+        return new ApiResponse<>(OK.value(), OK.name(), "success", null, LocalDateTime.now());
     }
 
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(HttpStatus.OK.name(), "success", data, LocalDateTime.now());
+        return new ApiResponse<>(OK.value(), OK.name(), "success", data, LocalDateTime.now());
     }
 
     public static <T> ApiResponse<T> success(String message) {
-        return new ApiResponse<>(HttpStatus.OK.name(), message, null, LocalDateTime.now());
+        return new ApiResponse<>(OK.value(), OK.name(), message, null, LocalDateTime.now());
     }
 
     public static <T> ApiResponse<T> success(T data, String message) {
-        return new ApiResponse<>(HttpStatus.OK.name(), message, data, LocalDateTime.now());
+        return new ApiResponse<>(OK.value(), OK.name(), message, data, LocalDateTime.now());
     }
 
     public static <T> ApiResponse<T> error(HttpStatus httpStatus) {
-        return new ApiResponse<>(httpStatus.name(), httpStatus.getReasonPhrase(), null, LocalDateTime.now());
+        return new ApiResponse<>(httpStatus.value(), httpStatus.name(), httpStatus.getReasonPhrase(), null,
+                LocalDateTime.now());
     }
 
     public static <T> ApiResponse<T> error(HttpStatus httpStatus, String message) {
-        return new ApiResponse<>(httpStatus.name(), message, null, LocalDateTime.now());
+        return new ApiResponse<>(httpStatus.value(), httpStatus.name(), message, null, LocalDateTime.now());
     }
 }
