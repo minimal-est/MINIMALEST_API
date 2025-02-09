31968d73077d9a5eb21274cc6f5334dd72293b02
31n5ang <31n5ang@gmail.com>
2025-02-09 18:10:02 +0900

    rename: 클래스 이름 명확하게 수정

--- src/main/java/kr/minimalest/core/domain/file/FileResourceApi.java
+++ src/main/java/kr/minimalest/core/domain/file/FileResourceApi.java
@@ -1,7 +1,8 @@
 package kr.minimalest.core.domain.file;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
+@Tag(name = "FileResource API", description = "파일 리소스 요청 관련 API 입니다.")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/file")
-public class FileProxyApi {
+public class FileResourceApi {
 
     private final FileService fileService;
 
+    @Operation(summary = "가상 URL을 통해 실제 리소스를 요청합니다.")
     @GetMapping("/proxy")
     public ResponseEntity<?> findProxyResource(
             @RequestParam("key") String key
