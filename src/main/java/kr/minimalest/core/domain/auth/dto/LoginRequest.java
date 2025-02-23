ca3cc153682ac659b19bc31442e3d4cb86a9cad4
31n5ang <31n5ang@gmail.com>
2025-02-23 11:05:49 +0900

    feat: Validation 추가

--- src/main/java/kr/minimalest/core/domain/auth/dto/LoginRequest.java
+++ src/main/java/kr/minimalest/core/domain/auth/dto/LoginRequest.java
@@ -1,10 +1,16 @@
 package kr.minimalest.core.domain.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
 
+    @NotEmpty
+    @Email
     private String email;
+
+    @NotEmpty
     private String rawPassword;
 }
