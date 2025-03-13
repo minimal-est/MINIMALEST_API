5e4acad0a24d6c3f707d29ae5d32af06e705b598
31n5ang <31n5ang@gmail.com>
2025-03-14 02:06:45 +0900

    feat: Validation 에러 형식 추가

---
+++ src/main/java/kr/minimalest/core/common/exception/ValidationError.java
@@ -0,0 +1,11 @@
+package kr.minimalest.core.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ValidationError {
+    private String field;
+    private String message;
+}
