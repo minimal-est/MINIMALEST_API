685e72e162a69ab4782e7d400f963aa63731d0d6
31n5ang <31n5ang@gmail.com>
2025-03-29 04:25:13 +0900

    refactor: 조회수 DB에 동기화하는 주기 '1일->3시간'으로 변경

--- src/main/java/kr/minimalest/core/domain/post/service/batch/PostHitBatch.java
+++ src/main/java/kr/minimalest/core/domain/post/service/batch/PostHitBatch.java
@@ -3,9 +3,11 @@ package kr.minimalest.core.domain.post.service.batch;
 import kr.minimalest.core.domain.post.service.PostHitCounter;
 import kr.minimalest.core.domain.post.service.PostHitService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PostHitBatch {
@@ -13,15 +15,21 @@ public class PostHitBatch {
     private final PostHitService postHitService;
     private final PostHitCounter postHitCounter;
 
-    // 매일 자정
-    @Scheduled(cron = "0 0 0 * * ?")
+    // 매일 3시간마다
+    @Scheduled(cron = "0 0 0/3 * * ?")
     public void saveAndClearHitCounts() {
         // DB에 조회수 동기화
-        postHitCounter.getHitCountStorage().getAllHitCounts().forEach(((postViewKey, atomicHitCount) -> {
-            postHitService.flushHitCount(postViewKey);
-        }));
+        try {
+            postHitCounter.getHitCountStorage().getAllHitCounts().forEach(((postViewKey, atomicHitCount) -> {
+                postHitService.flushHitCount(postViewKey);
+            }));
 
-        // IP 캐시 초기화
-        postHitService.clearIpChecker();
+            // IP 캐시 초기화
+            postHitService.clearIpChecker();
+
+            log.info("DB와 조회수 동기화 작업을 완료했습니다.");
+        } catch (Exception ex) {
+            log.error("DB에 조회수를 동기화시키는 과정에서 문제가 발생했습니다!", ex);
+        }
     }
 }
