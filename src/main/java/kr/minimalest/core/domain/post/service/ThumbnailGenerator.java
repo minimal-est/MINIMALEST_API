7ec4f95c5e9dbae0762533733329fb80a9bf5fca
31n5ang <31n5ang@gmail.com>
2025-04-04 21:36:59 +0900

    remove: 썸네일 생성 외부로 위임에 따른 불필요한 클래스 제거

--- src/main/java/kr/minimalest/core/domain/post/service/ThumbnailGenerator.java
+++
@@ -1,8 +0,0 @@
-package kr.minimalest.core.domain.post.service;
-
-import java.io.OutputStream;
-
-public interface ThumbnailGenerator {
-
-    void create(String imageUrl, String pureExt, OutputStream outputStream);
-}
