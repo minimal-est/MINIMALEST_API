32c4250839a7d39f5b6c28b226c5e27aa2673ae3
31n5ang <31n5ang@gmail.com>
2025-04-10 23:04:52 +0900

    refactor: Swagger 태그 제거

--- src/main/java/kr/minimalest/core/domain/file/FileResourceApi.java
+++ src/main/java/kr/minimalest/core/domain/file/FileResourceApi.java
@@ -1,7 +1,5 @@
 package kr.minimalest.core.domain.file;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "FileResource API", description = "파일 리소스 요청 관련 API 입니다.")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/file")
@@ -18,7 +15,6 @@ public class FileResourceApi {
 
     private final FileService fileService;
 
-    @Operation(summary = "가상 URL을 통해 실제 리소스를 요청합니다.")
     @GetMapping("/proxy")
     public ResponseEntity<?> findProxyResource(
             @RequestParam("key") String key
