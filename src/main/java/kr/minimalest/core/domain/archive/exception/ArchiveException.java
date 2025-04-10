fedbd847d78de5d44afc3bb3d0fb7c836cf147e2
31n5ang <31n5ang@gmail.com>
2025-04-10 22:43:44 +0900

    feat: Archive 예외 클래스 추가

---
+++ src/main/java/kr/minimalest/core/domain/archive/exception/ArchiveException.java
@@ -0,0 +1,19 @@
+package kr.minimalest.core.domain.archive.exception;
+
+public class ArchiveException extends RuntimeException{
+    public ArchiveException() {
+        super();
+    }
+
+    public ArchiveException(String message) {
+        super(message);
+    }
+
+    public ArchiveException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ArchiveException(Throwable cause) {
+        super(cause);
+    }
+}
