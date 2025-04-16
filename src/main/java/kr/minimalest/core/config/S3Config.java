0c3c3e683234c2434efdada6e09d7f4e6fff4064
31n5ang <31n5ang@gmail.com>
2025-04-16 17:02:44 +0900

    refactor: 테스트하기 쉽도록, s3 bucket을 설정 정보에서 주입하도록 변경

--- src/main/java/kr/minimalest/core/config/S3Config.java
+++ src/main/java/kr/minimalest/core/config/S3Config.java
@@ -1,6 +1,9 @@
 package kr.minimalest.core.config;
 
 import io.awspring.cloud.s3.*;
+import kr.minimalest.core.domain.file.storage.S3StorageResourceFinder;
+import kr.minimalest.core.domain.file.storage.StorageResourceFinder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -16,5 +19,13 @@ public class S3Config {
     @Bean
     public S3ObjectContentTypeResolver s3ObjectContentTypeResolver() {
         return new PropertiesS3ObjectContentTypeResolver();
+    }
+
+    @Bean
+    public StorageResourceFinder s3StorageResourceFinder(
+            @Value("${spring.cloud.aws.bucket.name}") String bucketName,
+            S3Operations s3Operations
+    ) {
+        return new S3StorageResourceFinder(bucketName, s3Operations);
     }
 }
--- src/main/java/kr/minimalest/core/domain/file/storage/S3StorageResourceFinder.java
+++ src/main/java/kr/minimalest/core/domain/file/storage/S3StorageResourceFinder.java
@@ -4,22 +4,34 @@ import io.awspring.cloud.s3.S3Operations;
 import io.awspring.cloud.s3.S3Resource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 @Slf4j
 @RequiredArgsConstructor
-@Component
 public class S3StorageResourceFinder implements StorageResourceFinder {
 
-    @Value("${spring.cloud.aws.bucket.name}")
-    private String BUCKET;
-
+    private final String bucketName;
     private final S3Operations s3Operations;
 
     @Override
     public StorageResource find(String key) {
-        S3Resource s3Resource = s3Operations.download(BUCKET, key);
-        return new S3StorageResource(s3Resource);
+        hasKey(key);
+        S3Resource resource = download(key);
+        return new S3StorageResource(resource);
+    }
+
+    private static void hasKey(String key) {
+        if (!StringUtils.hasText(key)) {
+            throw new IllegalStateException("key값이 비어있습니다!");
+        }
+    }
+
+    private S3Resource download(String key) {
+        try {
+            return s3Operations.download(bucketName, key);
+        } catch (Exception ex) {
+            log.error("S3에서 해당 key값의 리소스를 다운로드 중 실패 했습니다! key: {}", key, ex);
+            throw new StorageResourceDownloadException("S3에서 해당 key값의 리소스를 다운로드 중 실패 했습니다!", ex);
+        }
     }
 }
