23535145ab4c92da736c2555b2cdcc79c098b664
31n5ang <31n5ang@gmail.com>
2025-04-16 17:15:39 +0900

    feat: key값 검증하기

--- src/main/java/kr/minimalest/core/domain/file/FileVirtualUrlResolver.java
+++ src/main/java/kr/minimalest/core/domain/file/FileVirtualUrlResolver.java
@@ -2,6 +2,7 @@ package kr.minimalest.core.domain.file;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 @Component
 public class FileVirtualUrlResolver {
@@ -10,6 +11,13 @@ public class FileVirtualUrlResolver {
     private String VIRTUAL_REQUEST;
 
     public String resolve(String key) {
+        hasKey(key);
         return VIRTUAL_REQUEST + "?key=" + key;
+    }
+
+    private static void hasKey(String key) {
+        if (!StringUtils.hasText(key)) {
+            throw new IllegalStateException("key값이 빈 값 또는 null 입니다!");
+        }
     }
 }
