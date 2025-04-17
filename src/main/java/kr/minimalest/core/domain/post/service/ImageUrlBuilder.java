433178956f0fda30240c1bf8a05724077b36d2a9
31n5ang <31n5ang@gmail.com>
2025-04-17 23:34:17 +0900

    refactor: 프록시 URL 주입받도록 수정

--- src/main/java/kr/minimalest/core/domain/post/service/ImageUrlBuilder.java
+++ src/main/java/kr/minimalest/core/domain/post/service/ImageUrlBuilder.java
@@ -2,21 +2,21 @@ package kr.minimalest.core.domain.post.service;
 
 import jakarta.servlet.http.HttpServletRequest;
 import kr.minimalest.core.common.utils.ServerHostResolver;
+import kr.minimalest.core.domain.file.FileVirtualUrlResolver;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class ImageUrlBuilder {
 
-    private final HttpServletRequest request;
-
-    @Value("${file.proxy.url}")
-    private String proxyUrl;
+    private final FileVirtualUrlResolver fileVirtualUrlResolver;
+    private final ObjectProvider<HttpServletRequest> requestProvider;
 
     public String build(String key) {
+        HttpServletRequest request = requestProvider.getObject();
         String host = ServerHostResolver.getServerHost(request);
-        return host + proxyUrl + "?key=" + key;
+        return host + fileVirtualUrlResolver.resolve(key);
     }
 }
