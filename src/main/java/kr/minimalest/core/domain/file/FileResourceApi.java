8bd55ca620e73479bd96140d2fca95c39ce0bc3b
31n5ang <31n5ang@gmail.com>
2025-06-29 15:50:42 +0900

    refactor: S3 이미지 CloudFront를 통해 가져오도록 리팩토링

--- src/main/java/kr/minimalest/core/domain/file/FileResourceApi.java
+++ src/main/java/kr/minimalest/core/domain/file/FileResourceApi.java
@@ -2,21 +2,30 @@ package kr.minimalest.core.domain.file;
 
 import kr.minimalest.core.domain.file.storage.StorageResource;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/file")
 public class FileResourceApi {
 
+    @Value("${spring.cloud.aws.cloudfront.domain}")
+    private String cloudFrontDomain;
+
     private final FileService fileService;
 
-    @GetMapping("/proxy")
+    @Deprecated
+    @GetMapping("/proxy/direct")
     public ResponseEntity<?> findProxyResource(
             @RequestParam("key") String key
     ) {
@@ -27,5 +36,17 @@ public class FileResourceApi {
                 .header("Content-Type", resource.getContentType())
                 .header("Content-Disposition", "inline;")
                 .body(new InputStreamResource(resource.getInputStream()));
+    }
+
+    @GetMapping("/proxy")
+    public ResponseEntity<?> getCloudFrontResource(
+            @RequestParam("key") String key
+    ) {
+        String cloudFrontResourceUrl = cloudFrontDomain + "/" + key;
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(URI.create(cloudFrontResourceUrl));
+
+        return new ResponseEntity<>(httpHeaders, HttpStatus.FOUND);
     }
 }
