19e81687c7510a226b1d22788667623220dc74a2
31n5ang <31n5ang@gmail.com>
2025-06-29 21:52:49 +0900

    fix: 썸네일 생성 중 URL 리졸브 버그 수정

--- src/main/java/kr/minimalest/core/domain/post/service/ImageUrlBuilder.java
+++ src/main/java/kr/minimalest/core/domain/post/service/ImageUrlBuilder.java
@@ -1,10 +1,7 @@
 package kr.minimalest.core.domain.post.service;
 
-import jakarta.servlet.http.HttpServletRequest;
-import kr.minimalest.core.common.utils.ServerHostResolver;
 import kr.minimalest.core.domain.file.FileVirtualUrlResolver;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,11 +9,8 @@ import org.springframework.stereotype.Component;
 public class ImageUrlBuilder {
 
     private final FileVirtualUrlResolver fileVirtualUrlResolver;
-    private final ObjectProvider<HttpServletRequest> requestProvider;
 
     public String build(String key) {
-        HttpServletRequest request = requestProvider.getObject();
-        String host = ServerHostResolver.getServerHost(request);
-        return host + fileVirtualUrlResolver.resolve(key);
+        return fileVirtualUrlResolver.resolve(key);
     }
 }
